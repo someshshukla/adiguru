ed1f64a8775d79c62dc06c341c9ec4192ce7da63
Somesh Shukla <somesh13shukla@gmail.com>
2023-02-12 22:03:35 +0530

    edited the homepage a little bit

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
 import App from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const HomePage = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
+  
+  return (
+    <Box>
+      <Navbar/>
+      <Box
+      width="100%"
+      padding="2rem 6%">
+      </Box>
+    </Box>
+  )
+
+};
 
 
